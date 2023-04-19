225f1e8493cb371d4a3ca4d0a81c5483bce97588
cschulzk <christophschulzke@gmail.com>
2023-04-18 21:52:20 -0500

    Add github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  env: {
-    WEATHER_API_BASE_URL: "http://api.weatherapi.com/v1",
-    WEATHER_API_KEY: "494fe269c84142fd9b0235755231304"
-  },
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
