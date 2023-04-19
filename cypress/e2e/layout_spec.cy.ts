e45c773f2671b60a820359e957060cca6a04456e
cschulzk <christophschulzke@gmail.com>
2023-04-18 20:49:36 -0500

    Add cypress testing

---
+++ cypress/e2e/layout_spec.cy.ts
@@ -0,0 +1,25 @@
+describe('Layout', () => {
+  it('Successfully loads', () => {
+    cy.visit('/')
+  })
+  it('Has header', () => {
+    cy.visit('/')
+    cy.get('header')
+  })
+  it('Has footer', () => {
+    cy.visit('/')
+    cy.get('footer')
+  })
+  it('Correct title', () => {
+    cy.visit('/')
+    cy.get('header > h1').contains('Weather App')
+  })
+  it('Has unit preference selector', () => {
+    cy.visit('/')
+    cy.get('footer fieldset').get('legend').contains('Units')
+  })
+  it('Has search bar', () => {
+    cy.visit('/')
+    cy.get('input[id*="search-bar"]')
+  })
+})
